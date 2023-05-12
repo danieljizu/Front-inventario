df5dc060a04588da455069f83be638e16102c1e8
Daniel jimenez <Daniel_jimenez25@hotmail.com>
2023-05-12 13:51:54 -0500

    salvando ultimos cambios antes de actualizar angular material v 15.1

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,4 +1,3 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './pages/dashboard.component';
